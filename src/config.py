988dee6ec149e5eeac4dc39718cbeb9c8b64b203
edu0413 <97303260+edu0413@users.noreply.github.com>
2022-10-26 15:16:11 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ UPLOAD_FOLDER = 'templates/assets/img'
 MAIL_SERVER ='herse.dnsherse.com'
 MAIL_PORT = 465
 MAIL_USERNAME = 'geral@betbeans.pt'
-MAIL_PASSWORD = 'Pitcairnier1790'
+MAIL_PASSWORD = 'random_pass'
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
 
